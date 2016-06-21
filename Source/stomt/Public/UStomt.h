21e7ae46de75c6a1e21a4035f6c5d7604e7eebdf
DANI <daniel.schukies@gmail.com>
2016-06-21 14:54:28 +0200

    [Added] UStomt Class

---
+++ Source/stomt/Public/UStomt.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Object.h"
+#include "UStomt.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STOMT_API UUStomt : public UObject
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
