6854037848e5ee48a27e0a65d40b0ed52a94463b
Max Klenk <max@klenk.biz>
2018-04-16 16:03:43 +0200

    add UE/4.19 to pack script

--- pack.sh
+++ pack.sh
@@ -37,6 +37,19 @@ mv CONTRIBUTING.md Docs/CONTRIBUTING.md
 rm ../stomt-unreal-plugin-4-18.zip
 zip -r -X ../stomt-unreal-plugin-4-18.zip . -x *.git* -x push.sh -x *.DS_Store* -x LICENSE.md
 
+# next
+git add .
+git stash
+git checkout UE/4.19
+
+# move root files
+mv README.md Docs/README.md
+mv CONTRIBUTING.md Docs/CONTRIBUTING.md
+
+# zip
+rm ../stomt-unreal-plugin-4-19.zip
+zip -r -X ../stomt-unreal-plugin-4-19.zip . -x *.git* -x push.sh -x *.DS_Store* -x LICENSE.md
+
 # clean
 git add .
 git stash
