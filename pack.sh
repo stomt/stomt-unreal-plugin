0ee0e6478ffdd9dc113c91fe1688bcc80cff02d3
Max Klenk <max@klenk.biz>
2018-07-31 15:21:08 +0200

    update pack.sh for new Unreal version 4.20

--- pack.sh
+++ pack.sh
@@ -1,32 +1,6 @@
 # open
 git add .
 git stash
-git checkout UE/4.16
-
-# move root files
-mv README.md Docs/README.md
-mv CONTRIBUTING.md Docs/CONTRIBUTING.md
-
-# zip
-rm ../stomt-unreal-plugin-4-16.zip
-zip -r -X ../stomt-unreal-plugin-4-16.zip . -x *.git* -x push.sh -x *.DS_Store* -x LICENSE.md
-
-# next
-git add .
-git stash
-git checkout UE/4.17
-
-# move root files
-mv README.md Docs/README.md
-mv CONTRIBUTING.md Docs/CONTRIBUTING.md
-
-# zip
-rm ../stomt-unreal-plugin-4-17.zip
-zip -r -X ../stomt-unreal-plugin-4-17.zip . -x *.git* -x push.sh -x *.DS_Store* -x LICENSE.md
-
-# next
-git add .
-git stash
 git checkout UE/4.18
 
 # move root files
@@ -49,6 +23,19 @@ mv CONTRIBUTING.md Docs/CONTRIBUTING.md
 # zip
 rm ../stomt-unreal-plugin-4-19.zip
 zip -r -X ../stomt-unreal-plugin-4-19.zip . -x *.git* -x push.sh -x *.DS_Store* -x LICENSE.md
+
+# next
+git add .
+git stash
+git checkout UE/4.20
+
+# move root files
+mv README.md Docs/README.md
+mv CONTRIBUTING.md Docs/CONTRIBUTING.md
+
+# zip
+rm ../stomt-unreal-plugin-4-20.zip
+zip -r -X ../stomt-unreal-plugin-4-20.zip . -x *.git* -x push.sh -x *.DS_Store* -x LICENSE.md
 
 # clean
 git add .
