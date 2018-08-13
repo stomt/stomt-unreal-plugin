b0dc8bfcc26480720286e783b6e4f993e8b0faf0
Max Klenk <max@klenk.biz>
2018-08-13 15:20:43 +0200

    do not pack pack.sh itself

--- pack.sh
+++ pack.sh
@@ -9,7 +9,7 @@ mv CONTRIBUTING.md Docs/CONTRIBUTING.md
 
 # zip
 rm ../stomt-unreal-plugin-4-18.zip
-zip -r -X ../stomt-unreal-plugin-4-18.zip . -x *.git* -x push.sh -x *.DS_Store* -x LICENSE.md
+zip -r -X ../stomt-unreal-plugin-4-18.zip . -x *.git* -x pack.sh -x *.DS_Store* -x LICENSE.md
 
 # next
 git add .
@@ -22,7 +22,7 @@ mv CONTRIBUTING.md Docs/CONTRIBUTING.md
 
 # zip
 rm ../stomt-unreal-plugin-4-19.zip
-zip -r -X ../stomt-unreal-plugin-4-19.zip . -x *.git* -x push.sh -x *.DS_Store* -x LICENSE.md
+zip -r -X ../stomt-unreal-plugin-4-19.zip . -x *.git* -x pack.sh -x *.DS_Store* -x LICENSE.md
 
 # next
 git add .
@@ -35,7 +35,7 @@ mv CONTRIBUTING.md Docs/CONTRIBUTING.md
 
 # zip
 rm ../stomt-unreal-plugin-4-20.zip
-zip -r -X ../stomt-unreal-plugin-4-20.zip . -x *.git* -x push.sh -x *.DS_Store* -x LICENSE.md
+zip -r -X ../stomt-unreal-plugin-4-20.zip . -x *.git* -x pack.sh -x *.DS_Store* -x LICENSE.md
 
 # clean
 git add .
