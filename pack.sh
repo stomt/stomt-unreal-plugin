be26f5c837f8e54c2eb3812fb640c8f076346315
Max Klenk <max@klenk.biz>
2018-12-28 16:18:11 +0100

    add UE/4.21 branch in pack script

--- pack.sh
+++ pack.sh
@@ -1,4 +1,4 @@
-# open
+# open (4.18)
 git add .
 git stash
 git checkout UE/4.18
@@ -11,7 +11,8 @@ mv CONTRIBUTING.md Docs/CONTRIBUTING.md
 rm ../stomt-unreal-plugin-4-18.zip
 zip -r -X ../stomt-unreal-plugin-4-18.zip . -x *.git* -x pack.sh -x *.DS_Store* -x LICENSE.md -x Binaries/ -x Intermediate/ -x Binaries/**\* -x Intermediate/**\*
 
-# next
+
+# next (4.19)
 git add .
 git stash
 git checkout UE/4.19
@@ -24,7 +25,8 @@ mv CONTRIBUTING.md Docs/CONTRIBUTING.md
 rm ../stomt-unreal-plugin-4-19.zip
 zip -r -X ../stomt-unreal-plugin-4-19.zip . -x *.git* -x pack.sh -x *.DS_Store* -x LICENSE.md -x Binaries/ -x Intermediate/ -x Binaries/**\* -x Intermediate/**\*
 
-# next
+
+# next (4.20)
 git add .
 git stash
 git checkout UE/4.20
@@ -36,6 +38,21 @@ mv CONTRIBUTING.md Docs/CONTRIBUTING.md
 # zip
 rm ../stomt-unreal-plugin-4-20.zip
 zip -r -X ../stomt-unreal-plugin-4-20.zip . -x *.git* -x pack.sh -x *.DS_Store* -x LICENSE.md -x Binaries/ -x Intermediate/ -x Binaries/**\* -x Intermediate/**\*
+
+
+# next (4.21)
+git add .
+git stash
+git checkout UE/4.21
+
+# move root files
+mv README.md Docs/README.md
+mv CONTRIBUTING.md Docs/CONTRIBUTING.md
+
+# zip
+rm ../stomt-unreal-plugin-4-21.zip
+zip -r -X ../stomt-unreal-plugin-4-21.zip . -x *.git* -x pack.sh -x *.DS_Store* -x LICENSE.md -x Binaries/ -x Intermediate/ -x Binaries/**\* -x Intermediate/**\*
+
 
 # clean
 git add .
