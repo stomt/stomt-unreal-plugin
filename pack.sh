dc0635b1ff32b41b4c8c4b7c4c6c4c17922b2f03
Max Klenk <max@klenk.biz>
2019-04-03 10:48:23 +0200

    add UE4.22 in pack script

--- pack.sh
+++ pack.sh
@@ -54,6 +54,20 @@ rm ../stomt-unreal-plugin-4-21.zip
 zip -r -X ../stomt-unreal-plugin-4-21.zip . -x *.git* -x pack.sh -x *.DS_Store* -x LICENSE.md -x Binaries/ -x Intermediate/ -x Binaries/**\* -x Intermediate/**\*
 
 
+# next (4.22)
+git add .
+git stash
+git checkout UE/4.22
+
+# move root files
+mv README.md Docs/README.md
+mv CONTRIBUTING.md Docs/CONTRIBUTING.md
+
+# zip
+rm ../stomt-unreal-plugin-4-22.zip
+zip -r -X ../stomt-unreal-plugin-4-22.zip . -x *.git* -x pack.sh -x *.DS_Store* -x LICENSE.md -x Binaries/ -x Intermediate/ -x Binaries/**\* -x Intermediate/**\*
+
+
 # clean
 git add .
 git stash
