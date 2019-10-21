58268d62fac50ff43300f30d904f70b8eb2ff86f
Max Klenk <max@klenk.biz>
2019-10-21 23:42:36 +0200

    add and pack for UE 4.23

--- pack.sh
+++ pack.sh
@@ -1,45 +1,3 @@
-# open (4.18)
-git add .
-git stash
-git checkout UE/4.18
-
-# move root files
-mv README.md Docs/README.md
-mv CONTRIBUTING.md Docs/CONTRIBUTING.md
-
-# zip
-rm ../stomt-unreal-plugin-4-18.zip
-zip -r -X ../stomt-unreal-plugin-4-18.zip . -x *.git* -x pack.sh -x *.DS_Store* -x LICENSE.md -x Binaries/ -x Intermediate/ -x Binaries/**\* -x Intermediate/**\*
-
-
-# next (4.19)
-git add .
-git stash
-git checkout UE/4.19
-
-# move root files
-mv README.md Docs/README.md
-mv CONTRIBUTING.md Docs/CONTRIBUTING.md
-
-# zip
-rm ../stomt-unreal-plugin-4-19.zip
-zip -r -X ../stomt-unreal-plugin-4-19.zip . -x *.git* -x pack.sh -x *.DS_Store* -x LICENSE.md -x Binaries/ -x Intermediate/ -x Binaries/**\* -x Intermediate/**\*
-
-
-# next (4.20)
-git add .
-git stash
-git checkout UE/4.20
-
-# move root files
-mv README.md Docs/README.md
-mv CONTRIBUTING.md Docs/CONTRIBUTING.md
-
-# zip
-rm ../stomt-unreal-plugin-4-20.zip
-zip -r -X ../stomt-unreal-plugin-4-20.zip . -x *.git* -x pack.sh -x *.DS_Store* -x LICENSE.md -x Binaries/ -x Intermediate/ -x Binaries/**\* -x Intermediate/**\*
-
-
 # next (4.21)
 git add .
 git stash
@@ -66,6 +24,20 @@ mv CONTRIBUTING.md Docs/CONTRIBUTING.md
 # zip
 rm ../stomt-unreal-plugin-4-22.zip
 zip -r -X ../stomt-unreal-plugin-4-22.zip . -x *.git* -x pack.sh -x *.DS_Store* -x LICENSE.md -x Binaries/ -x Intermediate/ -x Binaries/**\* -x Intermediate/**\*
+
+
+# next (4.23)
+git add .
+git stash
+git checkout UE/4.23
+
+# move root files
+mv README.md Docs/README.md
+mv CONTRIBUTING.md Docs/CONTRIBUTING.md
+
+# zip
+rm ../stomt-unreal-plugin-4-23.zip
+zip -r -X ../stomt-unreal-plugin-4-23.zip . -x *.git* -x pack.sh -x *.DS_Store* -x LICENSE.md -x Binaries/ -x Intermediate/ -x Binaries/**\* -x Intermediate/**\*
 
 
 # clean
